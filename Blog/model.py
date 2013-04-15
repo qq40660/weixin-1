e4cf724fc945e0668401c6bcb160e3850c1fa593
evantre <625455600@qq.com>
2013-04-16 00:54:14 +0800

    big changes, add comment, modify styles and structure

--- Blog/model.py
+++ Blog/model.py
@@ -1,13 +1,15 @@
 import web, datetime
 
-# 改为对应数据 user, pw
-db = web.database(host='127.0.0.1', dbn='mysql', db='blog', user='', pw='')
+db = web.database(host='127.0.0.1', dbn='mysql', db='blog', user='justin', pw='5201314')
 
-def get_posts():
+def get_contents():
     return db.select('bbs', order='id DESC')
 
-def get_post(id):
+def get_content(id):
     try:
         return db.select('bbs', where='id=$id', vars=locals())[0]
     except IndexError:
         return None
+
+def get_comment(id):
+    return db.select('bbscomment', where='articleId=$id', vars=locals())
