d161e2a0ab9eac49749bb067fbd0b385018baf18
evantre <625455600@qq.com>
2013-04-17 00:51:02 +0800

    simple pagination

--- Blog/blog.py
+++ Blog/blog.py
@@ -4,6 +4,7 @@ import re
 
 urls = (
     '/', 'Index',
+    '/(\d+)', 'Pagination',
     '/view/(\d+)', 'View'
 )
 
@@ -15,8 +16,24 @@ render = web.template.render('templates', base='base', globals=t_globals)
 
 class Index:
     def GET(self):
-        post_contents = model.get_contents()
-        return render.index(post_contents)
+        return web.seeother('/1')
+
+class Pagination:
+    def GET(self, page=1):
+        page = int(page)
+        per_page = 10
+        offset = (page - 1) * per_page
+
+        post_contents = model.get_page(per_page, offset)
+        content_count = model.get_content_count()
+
+        count_of_pages = content_count.count / per_page
+        if content_count.count % per_page > 0:
+            count_of_pages += 1
+        if page > count_of_pages:
+            raise web.seeother('/')
+        else:
+            return render.index(post_contents=post_contents, count_of_pages=count_of_pages)
 
 class View:
     def GET(self, id):
