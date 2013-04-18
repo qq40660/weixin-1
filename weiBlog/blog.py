4f069225588ce5aceae0bcade4d91e8306e5d18d
shahuwang <shahuwang@qq.com>
2013-04-18 15:40:56 +0800

    改名

---
+++ weiBlog/blog.py
@@ -0,0 +1,48 @@
+﻿import os,sys
+path = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(path)
+import web
+import model
+import re
+
+urls = (
+    '/', 'Index',
+    '/(\d+)', 'Index',
+    '/view/(\d+)', 'View'
+)
+
+t_globals = {
+    'datestr': web.datestr,
+    're': re
+}
+render = web.template.render(path+'/templates/', base='base', globals=t_globals)
+
+class Index:
+    def GET(self, page=1):
+        page = int(page)
+        per_page = 10 
+        offset = (page - 1) * per_page
+
+        post_contents = model.get_page(per_page, offset) 
+        content_count = model.get_content_count() 
+
+        count_of_pages = content_count.count / per_page + 1
+
+        if page > count_of_pages:
+            raise web.seeother('/')
+        else:
+            return render.index(post_contents, count_of_pages, curr_page=page)
+
+class View:
+    def GET(self, id):
+        post_content = model.get_content(int(id))
+        post_comments = model.get_comment(int(id))
+        return render.view(post_content, post_comments)
+
+
+app = web.application(urls,globals(),autoreload=False)
+application = app.wsgifunc()
+
+if __name__ == '__main__':
+    app.run()
---
+++ weiBlog/model.py
@@ -0,0 +1,25 @@
+import os,sys
+path = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(path)
+import web, datetime
+
+db = web.database(host='127.0.0.1', dbn='mysql', db='blog', user='rickey', pw='5201314')
+
+def get_contents():
+    return db.select('bbs', order='id DESC')
+
+def get_content(id):
+    try:
+        return db.select('bbs', where='id=$id', vars=locals())[0]
+    except IndexError:
+        return None
+
+def get_page(per_page, offset):
+    return db.select('bbs', order='id DESC', offset=offset, limit=per_page)
+
+def get_content_count():
+    return db.query("SELECT COUNT(*) AS count FROM bbs")[0]
+
+def get_comment(id):
+    return db.select('bbscomment', where='articleId=$id', vars=locals())
